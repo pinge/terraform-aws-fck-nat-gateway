ab7726fbf3a36db151c900409e974b8f3cec6474
Nuno Pinge <nuno@boxtobox.app>
2023-08-12 18:31:42 +0200

    refactor network interface requests + lifecyle actions

--- user_data.sh
+++ user_data.sh
@@ -4,8 +4,8 @@ echo "eni_id=${eni_id}" >> /etc/fck-nat.conf
 echo "asg_name=${asg_name}" >> /etc/fck-nat.conf
 echo "asg_hook_name=${asg_hook_name}" >> /etc/fck-nat.conf
 
-# add single quotes around EOF to prevent parameter/arithmetic expansion and command substitution
-cat >~/heal.sh <<'EOF'
+# use single quotes around EOF to prevent parameter/arithmetic expansion and command substitution
+cat >~/lifecycle.sh <<'EOF'
 #!/usr/bin/env bash
 
 . /etc/fck-nat.conf
@@ -30,6 +30,13 @@ function get_ami_launch_index {
     echo $(curl -s -H "X-aws-ec2-metadata-token: $1" http://169.254.169.254/latest/meta-data/ami-launch-index)
 }
 
+function get_network_attachment_id {
+    echo $(aws ec2 describe-network-interface-attribute --region $1 --network-interface-id $2 --attribute attachment \
+        | grep -E -i -o 'AttachmentId": "(eni-attach-[a-z0-9]+)"' \
+        | cut -f 2 -d ' ' \
+        | tr -d '"')
+}
+
 function get_asg_lifecycle_state {
     echo $(aws autoscaling describe-auto-scaling-instances \
         --region $1 \
@@ -39,81 +46,56 @@ function get_asg_lifecycle_state {
     | tr -d '"')
 }
 
+function complete_lifecycle_action {
+    aws autoscaling complete-lifecycle-action \
+        --region $1 \
+        --auto-scaling-group-name $2 \
+        --lifecycle-hook-name $3 \
+        --instance-id $4 \
+        --lifecycle-action-result $5
+}
+
+function detach_network_interface {
+    attachment_id=$(get_network_attachment_id $1 $2)
+    if [ -n "$attachment_id" ]; then
+        aws ec2 detach-network-interface \
+            --region $1 \
+            --attachment-id $attachment_id \
+            --force
+        # wait until network interface is completely detached
+        while [ -n "$attachment_id" ]; do
+            attachment_id=$(get_network_attachment_id $1 $2)
+            sleep 0.5
+        done
+    fi
+}
+
 token=$(get_token)
 region=$(get_region $token)
 instance_id=$(get_instance_id $token)
 lifecycle_state=$(get_asg_lifecycle_state $region $instance_id)
 
 # check for Pending:Wait lifecycle to start reattaching the network interface to this instance
-if [[ "$lifecycle_state" == "Pending:Wait" && ! -f ~/heal.lock ]]; then
-
-    touch ~/heal.lock
-
-    # get ENI attachment id
-    attachment_id=$(aws ec2 describe-network-interface-attribute \
-        --region $region \
-        --attribute attachment \
-        --network-interface-id $eni_id \
-        | grep -E -i -o 'AttachmentId": "(eni-attach-[a-z0-9]+)"' \
-        | cut -f 2 -d ' ' \
-        | tr -d '"')
-
-    # force detaching the ENI
-    if [ -n "$attachment_id" ]; then
-        aws ec2 detach-network-interface \
-            --region $region \
-            --attachment-id $attachment_id \
-            --force
-    fi
-
-    # complete asg lifecycle action so the instance can transition from 'Pending:Wait' to 'InService'
-    aws autoscaling complete-lifecycle-action \
-        --lifecycle-hook-name $asg_hook_name \
-        --auto-scaling-group-name $asg_name \
-        --lifecycle-action-result CONTINUE \
-        --instance-id $instance_id \
-        --region $region
-
-    # wait until network interface is completely detached
-    while [ -n "$attachment_id" ]; do
-        attachment_id=$(aws ec2 describe-network-interface-attribute \
-            --region $region \
-            --attribute attachment \
-            --network-interface-id $eni_id \
-            | grep -E -i -o 'AttachmentId": "(eni-attach-[a-z0-9]+)"' \
-            | cut -f 2 -d ' ' \
-            | tr -d '"')
-        sleep 0.5
-    done
-
-    # restart fck-nat so the ENI is attached to the instance and routes are configured properly
+if [[ "$lifecycle_state" == "Pending:Wait" && ! -f ~/lifecycle.lock ]]; then
+    touch ~/lifecycle.lock
+    detach_network_interface $region $eni_id
     /sbin/service fck-nat restart
-
     crontab -r
-
+    complete_lifecycle_action $region $asg_name $asg_hook_name $instance_id CONTINUE
 fi
 
 # check for Warmed:Pending:Wait lifecycle so the instance can transition to Warmed:Running
-if [[ "$lifecycle_state" == "Warmed:Pending:Wait" && ! -f ~/warmed.lock ]]; then
-
-    touch ~/warmed.lock
-
-    # complete asg lifecycle action so the instance can be added to the warmed pool
-    aws autoscaling complete-lifecycle-action \
-        --lifecycle-hook-name $asg_hook_name \
-        --auto-scaling-group-name $asg_name \
-        --lifecycle-action-result CONTINUE \
-        --instance-id $instance_id \
-        --region $region
-
+if [[ "$lifecycle_state" == "Warmed:Pending:Wait" && ! -f ~/lifecycle.warmed.lock ]]; then
+    touch ~/lifecycle.warmed.lock
+    complete_lifecycle_action $region $asg_name $asg_hook_name $instance_id CONTINUE
 fi
 
 EOF
 
-chmod +x ~/heal.sh
+chmod +x ~/lifecycle.sh
 
 # add crontab to check lifecycle state every 5 seconds
 crontab<<EOF
 $(crontab -l)
-* * * * * for i in {0..11}; do ~/heal.sh & sleep 5; done; touch ~/heal.sh
+* * * * * for i in {0..11}; do ~/lifecycle.sh & sleep 5; done; touch ~/lifecycle.sh
 EOF
